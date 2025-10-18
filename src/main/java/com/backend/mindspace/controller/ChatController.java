a4299eb35f18c5cd7f0998bfd8dac3c7b6c0ee8f
sabin <sabinkarki064@gmail.com>
2025-10-18 20:54:10 +0545

    user auth and sessions

--- src/main/java/com/backend/mindspace/controller/ChatController.java
+++ src/main/java/com/backend/mindspace/controller/ChatController.java
@@ -3,16 +3,18 @@ package com.backend.mindspace.controller;
 
 import com.backend.mindspace.dto.ChatRequest;
 import com.backend.mindspace.dto.ChatResponse;
+import com.backend.mindspace.entity.User;
 import com.backend.mindspace.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/chat")
+@RequestMapping("/api/v1/chat")
 public class ChatController {
 
 
@@ -24,8 +26,8 @@ public class ChatController {
     }
 
     @PostMapping("/ask")
-    private ResponseEntity<ChatResponse> askQuestion(@RequestBody ChatRequest chatRequest){
-        ChatResponse response = chatService.askQuestion(chatRequest.getQuestion(),chatRequest.getSourceId());
+    private ResponseEntity<ChatResponse> askQuestion(@AuthenticationPrincipal User user ,@RequestBody ChatRequest chatRequest){
+        ChatResponse response = chatService.askQuestion(user,chatRequest.getQuestion(),chatRequest.getSourceId());
         return ResponseEntity.ok(response);
     }
 }
