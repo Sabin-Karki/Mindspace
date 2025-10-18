cb4f0851c03a6fd36ca535be5971d8c8a7633422
sabin <sabinkarki064@gmail.com>
2025-10-18 20:55:10 +0545

    optional method

--- src/main/java/com/backend/mindspace/repository/ChatSessionRepository.java
+++ src/main/java/com/backend/mindspace/repository/ChatSessionRepository.java
@@ -2,6 +2,8 @@ package com.backend.mindspace.repository;
 
 import com.backend.mindspace.entity.ChatSession;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public interface ChatSessionRepository extends JpaRepository<ChatSession,Long> {
+public interface ChatSessionRepository extends JpaRepository<ChatSession, Long> {
+    Optional<ChatSession> findByUserIdAndSourceId(Long userId, Long sourceId);
 }
