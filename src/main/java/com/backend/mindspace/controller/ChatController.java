292ff632e144b8de46da6e78d9085e6f23a6cbc1
sabin <sabinkarki064@gmail.com>
2025-10-19 22:11:48 +0545

    added chatsession

--- src/main/java/com/backend/mindspace/controller/ChatController.java
+++ src/main/java/com/backend/mindspace/controller/ChatController.java
@@ -3,15 +3,15 @@ package com.backend.mindspace.controller;
 
 import com.backend.mindspace.dto.ChatRequest;
 import com.backend.mindspace.dto.ChatResponse;
+import com.backend.mindspace.entity.ChatSession;
 import com.backend.mindspace.entity.User;
+import com.backend.mindspace.repository.ChatSessionRepository;
 import com.backend.mindspace.service.ChatService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api/v1/chat")
@@ -19,15 +19,26 @@ public class ChatController {
 
 
     private final ChatService chatService;
+    private final ChatSessionRepository chatSessionRepository;
 
 
-    public ChatController(ChatService chatService) {
+    public ChatController(ChatService chatService, ChatSessionRepository chatSessionRepository) {
         this.chatService = chatService;
+        this.chatSessionRepository = chatSessionRepository;
     }
 
-    @PostMapping("/ask")
-    private ResponseEntity<ChatResponse> askQuestion(@AuthenticationPrincipal User user ,@RequestBody ChatRequest chatRequest){
-        ChatResponse response = chatService.askQuestion(user,chatRequest.getQuestion(),chatRequest.getSourceId());
+    @PostMapping
+    public ResponseEntity<ChatSession> createChatSession(@AuthenticationPrincipal User user) {
+        ChatSession newSession = new ChatSession();
+        newSession.setUser(user);
+        newSession.setCreatedAt(LocalDate.now());
+        ChatSession savedSession = chatSessionRepository.save(newSession);
+        return ResponseEntity.ok(savedSession);
+    }
+
+    @PostMapping("/{sessionId}/ask")
+    public  ResponseEntity<ChatResponse> askQuestion(@AuthenticationPrincipal User user, @PathVariable Long sessionId, @RequestBody ChatRequest chatRequest){
+        ChatResponse response = chatService.askQuestion(user, chatRequest.getQuestion(), sessionId);
         return ResponseEntity.ok(response);
     }
 }
