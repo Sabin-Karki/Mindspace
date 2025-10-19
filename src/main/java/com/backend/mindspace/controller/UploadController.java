f118e8cb40f3d7fb9da58d31e02c9b30bbfd3b36
sabin <sabinkarki064@gmail.com>
2025-10-19 22:13:53 +0545

    api for user upload

--- src/main/java/com/backend/mindspace/controller/UploadController.java
+++ src/main/java/com/backend/mindspace/controller/UploadController.java
@@ -1,35 +1,34 @@
 package com.backend.mindspace.controller;
 
 import com.backend.mindspace.dto.UploadResponse;
+import com.backend.mindspace.entity.User;
 import com.backend.mindspace.service.UploadService;
 import org.apache.tika.exception.TikaException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/upload")
+@RequestMapping("/api/v1/upload")
 public class UploadController {
 
     private final UploadService uploadService;
     public UploadController(UploadService uploadService){
         this.uploadService=uploadService;
     }
-    @PostMapping("/content")
-    public ResponseEntity<UploadResponse> uploadContent(@RequestParam(name="file",required = false)MultipartFile file, @RequestParam(name="textContent",required = false) String testContent ) throws IOException, TikaException {
+    @PostMapping("/content/{sessionId}")
+    public ResponseEntity<UploadResponse> uploadContent(@AuthenticationPrincipal User user, @PathVariable Long sessionId, @RequestParam(name="file",required = false)MultipartFile file, @RequestParam(name="textContent",required = false) String testContent ) throws IOException, TikaException {
         if(file==null || file.isEmpty()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
         else if(testContent==null || testContent.isEmpty()){
             return ResponseEntity.badRequest().build();
             }
-             UploadResponse response = uploadService.processContent(file,testContent);
+             UploadResponse response = uploadService.processContent(user,sessionId,file,testContent);
         return ResponseEntity.ok(response);
 
     }
