95b06817a0afd9b210a5ff801109d74e294e5180
sabin <sabinkarki064@gmail.com>
2025-10-22 19:41:11 +0545

    successfull authentication

---
+++ src/main/java/com/backend/mindspace/service/AuthenticationService.java
@@ -0,0 +1,50 @@
+package com.backend.mindspace.service;
+
+import com.backend.mindspace.dto.JwtAuthenticationResponse;
+import com.backend.mindspace.dto.SignUpRequest;
+import com.backend.mindspace.dto.SigninRequest;
+import com.backend.mindspace.entity.User;
+import com.backend.mindspace.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public JwtAuthenticationResponse signup(SignUpRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .createdAt(LocalDate.now())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+        userRepository.save(user);
+        var jwt = jwtService.generateToken(user);
+        var response = new JwtAuthenticationResponse();
+        response.setToken(jwt);
+        return response;
+    }
+
+    public JwtAuthenticationResponse signin(SigninRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid email or password."));
+        var jwt = jwtService.generateToken(user);
+        var response = new JwtAuthenticationResponse();
+        response.setToken(jwt);
+        return response;
+    }
+}
