45473c21b4545622c742c2d3509ea7ed05084199
sabin <sabinkarki064@gmail.com>
2025-10-23 15:40:50 +0545

    added dto as return type and get requests

--- src/main/java/com/backend/mindspace/controller/FlashCardController.java
+++ src/main/java/com/backend/mindspace/controller/FlashCardController.java
@@ -1,12 +1,13 @@
 package com.backend.mindspace.controller;
 
+import com.backend.mindspace.dto.CardRequestDTO;
+import com.backend.mindspace.dto.CardResponse;
 import com.backend.mindspace.entity.FlashCard;
+import com.backend.mindspace.entity.FlashCardOverview;
 import com.backend.mindspace.service.FlashCardService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,14 +20,37 @@ public class FlashCardController {
         this.flashCardService = flashCardService;
     }
 
-    @PostMapping("/flash-card/{sourceId}")
-    private ResponseEntity<List<FlashCard>> generateFlashCard(@PathVariable(name = "sourceId") Long sourceId) {
+    @PostMapping("/flash-card/{sessionId}")
+    private ResponseEntity<CardResponse> generateFlashCard(@PathVariable(name = "sessionId") Long sessionId, @RequestBody CardRequestDTO cardRequestDTO) {
         try {
-            List<FlashCard> cards = flashCardService.generateAndSaveCards(sourceId);
+            CardResponse cards = flashCardService.generateAndSaveCards(sessionId,cardRequestDTO.getSourceIds());
             return ResponseEntity.ok(cards);
         }catch(Exception e){
             e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
+
+    @GetMapping("/flash-card/{cardId}")
+    private ResponseEntity<CardResponse> getFlashCardOverviewById(@PathVariable(name = "cardId") Long id){
+        try{
+            CardResponse cardOverview = flashCardService.getCardOverviewById(id);
+            return ResponseEntity.ok(cardOverview);
+        }catch(RuntimeException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/flash-card/session/{sessionId}")
+    private ResponseEntity <List<CardResponse>> getFlashCardOverviewBySessionid(@PathVariable(name = "sessionId") Long id) {
+        try {
+            List<CardResponse> cardOverview = flashCardService.getCardOverviewBySessionId(id);
+            return ResponseEntity.ok(cardOverview);
+        }catch (RuntimeException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+
+
 }
