77e4a03a14183a877b39136f77e9a35f9a8b6ef8
sabin <sabinkarki064@gmail.com>
2025-11-18 09:42:56 +0545

    report repo

--- backend/src/main/java/com/backend/mindspace/repository/ReportRepository.java
+++ backend/src/main/java/com/backend/mindspace/repository/ReportRepository.java
@@ -3,6 +3,10 @@ package com.backend.mindspace.repository;
 import com.backend.mindspace.entity.ReportGeneration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ReportRepository extends JpaRepository<ReportGeneration,Long> {
 
+   List<ReportGeneration> findBySource_ChatSession_SessionIdAndSource_User_UserId(Long sessionId, Long currentUserId);
 }
