294fdcdcee0d626e87b1b34c5a22a06697975b52
sabin <sabinkarki064@gmail.com>
2025-12-04 18:21:59 +0545

    minor fix

--- backend/src/main/java/com/backend/mindspace/repository/ReportRepository.java
+++ backend/src/main/java/com/backend/mindspace/repository/ReportRepository.java
@@ -4,9 +4,8 @@ import com.backend.mindspace.entity.ReportGeneration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ReportRepository extends JpaRepository<ReportGeneration,Long> {
 
-   List<ReportGeneration> findBySource_ChatSession_SessionIdAndSource_User_UserId(Long sessionId, Long currentUserId);
+   List<ReportGeneration> findBySources_ChatSession_SessionIdAndSources_User_UserId(Long sessionId, Long currentUserId);
 }
