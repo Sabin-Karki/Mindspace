c827a6e973e502ef19385843c02b2e2439fde03d
sabin <sabinkarki064@gmail.com>
2025-12-28 13:25:23 +0545

    quiz generator

--- frontend/src/components/studio/quiz/QuizGenerator.tsx
+++ frontend/src/components/studio/quiz/QuizGenerator.tsx
@@ -1,12 +1,48 @@
+import { toast } from "sonner";
+import { useEffect,useState } from "react";
+import { useQuizStore } from "../../../store/quizStore";
+import { useSessionStore } from "../../../store/sessionStore";
+import { generateQuiz } from "../../../api/quizApi";
+import type  { IQuizOverviewResponse } from "../../../types";
+
 const QuizGenerator = () =>{
 
-  return(
-    <>
-    <div>
-      Quiz
-    </div>
-    </>
-  )
+    //so for quiz generator,i will need according to the backend i designed->sessionId in path variable and selectedSourceIds[] in requestbody
+
+    const sessionId = useSessionStore((state)=>state.sessionId);
+    const selectedSourceIds= useSessionStore((state)=>state.selectedSourceIds);
+
+    const addQuiz = useQuizStore((state)=>state.addQuiz);
+
+    const[error,setError] = useState<string|null>(null);
+    const[loading,setIsLoading]=useState(false);
+
+    const handleGenerateQuizOverview=async()=>{
+      try{
+       setError(null);
+       setIsLoading(true);
+       if(!sessionId)return;
+
+       
+        const response : IQuizOverviewResponse= await generateQuiz(sessionId,selectedSourceIds);
+        console.log(response);
+        addQuiz(response);
+       
+      }catch(err:any){
+          const serverMessage=err?.response?.data?.message;
+          const axiosMessage=err?.message;
+          const message=serverMessage||axiosMessage||"Error generating Quiz . Please try again !";
+          setError(message);
+          toast.error(message);
+      }finally{
+        setIsLoading(false);
+      }
+    }
+    return (
+      <>
+      <div onClick={handleGenerateQuizOverview}>Quiz </div>
+      </>
+    )
 }
 
 export default QuizGenerator;
