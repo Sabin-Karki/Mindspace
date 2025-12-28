aa2dbeb88690415e09d08f4cc12868875093485b
sabin <sabinkarki064@gmail.com>
2025-12-28 13:27:54 +0545

    delete component (quiz)

---
+++ frontend/src/components/studio/quiz/DeleteQuizOverview.tsx
@@ -0,0 +1,41 @@
+import { toast } from "sonner";
+import { deleteQuiz } from "../../../api/quizApi";
+import { useQuizStore } from "../../../store/quizStore";
+
+ const DeleteQuizOverview = ({quizId,closeDeleteModal}:{quizId:number,closeDeleteModal:()=>void})=>{
+    const removeQuiz = useQuizStore((state)=>state.removeQuiz);
+
+    //delete quiz overview
+    const handleDeleteQuizOverview =async()=>{
+        try{
+            await deleteQuiz(quizId);
+            toast.success("Quiz deleted successfully");
+            
+            //update state 
+            removeQuiz(quizId); //remove from global state
+            toast.success("Quiz deleted successfully") ;
+        
+        }catch(error){
+            console.log(error);
+            toast.error("Failed to delete quiz");
+
+        }
+    }
+
+    return (
+        <>
+        <div onClick={(e)=>e.stopPropagation()}
+         className="bg-gray-800 rounded-lg p-4 w-full flex flex-col text-white">
+         
+         <div className="p-4" >Are you sure you want to delete this quiz ? </div>
+         <div className="flex justify-between items-center">
+            <button onClick={()=>{handleDeleteQuizOverview();closeDeleteModal();}}>Delete </button>
+            <button onClick={closeDeleteModal}>Cancel</button>
+         </div>
+
+         </div>
+        </>
+    )
+}
+
+export default DeleteQuizOverview;
