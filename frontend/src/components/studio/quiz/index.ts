578c6e8abd68cfa9996c1552c88040bd82e48c5f
sabin <sabinkarki064@gmail.com>
2025-12-28 13:27:12 +0545

    exports

--- frontend/src/components/studio/quiz/index.ts
+++ frontend/src/components/studio/quiz/index.ts
@@ -1,3 +1,6 @@
 export {default as QuizList} from './QuizList';
 export {default as QuizGet} from './QuizGet';
 export {default as QuizGenerator} from './QuizGenerator';
+export {default as RenameQuizOverview} from './RenameQuizOverview';
+export {default as DeleteQuizOverview} from './DeleteQuizOverview';
+export {default as QuizViewer} from './QuizViewer';
