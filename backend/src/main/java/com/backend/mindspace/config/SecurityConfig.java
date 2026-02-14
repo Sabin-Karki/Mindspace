fdbe9306a68c8414f40ab71c15aed9f7375df317
sabin <sabinkarki064@gmail.com>
2026-02-14 13:16:51 +0545

    port change

--- backend/src/main/java/com/backend/mindspace/config/SecurityConfig.java
+++ backend/src/main/java/com/backend/mindspace/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8081"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
